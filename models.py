6b63309c2f2a133a1983700122bff45bf3e8e2a4
Hosea <karanja.mungai23@gmail.com>
2025-03-04 09:23:46 +0300

    adds db columns

--- models.py
+++ models.py
@@ -54,3 +54,22 @@ class User(db.Model):
             "role": self.role.name if self.role else None,
             "scanned": [scanned.to_dict()for scanned in self.scanned]
         }
+
+class Role(db.Model):
+    __tablename__ = "roles"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    permissions = db.Column(db.ARRAY(db.String), default=[])
+
+    users = db.relationship("User", back_populates="role")
+
+    def __repr__(self):
+        return f"<Role {self.name}>"
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "name": self.name,
+            "permissions": self.permissions
+        }        
